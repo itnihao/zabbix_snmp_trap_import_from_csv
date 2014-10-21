075ac596ff6e8bd43843397e9383633355047df0
Zubair AHMED <zubayr.a@gmail.com>
2014-10-21 15:36:37 +0530

    reformated.

--- zabbix_snmp_trap_import_from_csv.py
+++ zabbix_snmp_trap_import_from_csv.py
@@ -2,7 +2,6 @@
 
 __author__ = 'ahmed'
 
-
 import csv
 import sys
 from logging import exception
@@ -19,30 +18,31 @@ import argparse
 
 # dictionary to hold IANA SMI numbers.
 iana_smi_numbers = {
-                '1': 'iso',
-                '1.3': 'org',
-                '1.3.6': 'dod',
-                '1.3.6.1': 'internet',
-                '1.3.6.1.1': 'directory',
-                '1.3.6.1.2': 'mgmt',
-                '1.3.6.1.2.1': 'mib-2',
-                '1.3.6.1.2.1.2.2.1.3': 'ifType',
-                '1.3.6.1.2.1.10': 'transmission',
-                '1.3.6.1.2.1.10.23': 'transmissionppp',
-                '1.3.6.1.2.1.27': 'application',
-                '1.3.6.1.2.1.28': 'mta',
-                '1.3.6.1.2.2': 'pib',
-                '1.3.6.1.3': 'experimental',
-                '1.3.6.1.4': 'private',
-                '1.3.6.1.4.1': 'enterprises',
-                '1.3.6.1.5': 'security',
-                '1.3.6.1.6': 'SNMPv2',
-                '1.3.6.1.6.1': 'snmpDomains',
-                '1.3.6.1.6.2': 'snmpProxys',
-                '1.3.6.1.6.3': 'snmpModules',
-                '1.3.6.1.7': 'mail',
-                '1.3.6.1.8': 'features'
-                }
+    '1': 'iso',
+    '1.3': 'org',
+    '1.3.6': 'dod',
+    '1.3.6.1': 'internet',
+    '1.3.6.1.1': 'directory',
+    '1.3.6.1.2': 'mgmt',
+    '1.3.6.1.2.1': 'mib-2',
+    '1.3.6.1.2.1.2.2.1.3': 'ifType',
+    '1.3.6.1.2.1.10': 'transmission',
+    '1.3.6.1.2.1.10.23': 'transmissionppp',
+    '1.3.6.1.2.1.27': 'application',
+    '1.3.6.1.2.1.28': 'mta',
+    '1.3.6.1.2.2': 'pib',
+    '1.3.6.1.3': 'experimental',
+    '1.3.6.1.4': 'private',
+    '1.3.6.1.4.1': 'enterprises',
+    '1.3.6.1.5': 'security',
+    '1.3.6.1.6': 'SNMPv2',
+    '1.3.6.1.6.1': 'snmpDomains',
+    '1.3.6.1.6.2': 'snmpProxys',
+    '1.3.6.1.6.3': 'snmpModules',
+    '1.3.6.1.7': 'mail',
+    '1.3.6.1.8': 'features'
+}
+
 
 def get_smi_number_to_name(smi_number):
     if smi_number not in iana_smi_numbers:
@@ -52,17 +52,17 @@ def get_smi_number_to_name(smi_number):
         return iana_smi_numbers[smi_number]
     pass
 
+
 # --------------------------------------------------------
 # Generate Complete Export/Import XML File
 # --------------------------------------------------------
 def generate_items_xml_file_complete(alarm_list, host_name, host_group_name, host_interface):
-
     zabbix_export = Element('zabbix_export')
     version = SubElement(zabbix_export, 'version')
     version.text = '2.0'
 
     fmt = '%Y-%m-%dT%H:%M:%SZ'
-    date =  SubElement(zabbix_export, 'date')
+    date = SubElement(zabbix_export, 'date')
     date.text = datetime.datetime.now().strftime(fmt)
 
     groups = SubElement(zabbix_export, 'groups')
@@ -75,7 +75,7 @@ def generate_items_xml_file_complete(alarm_list, host_name, host_group_name, hos
     host_under_host = SubElement(host_under_hosts, 'host')
     host_under_host.text = host_name.upper()
 
-    name_under_host = SubElement(host_under_hosts,'name')
+    name_under_host = SubElement(host_under_hosts, 'name')
     name_under_host.text = host_name.upper()
     SubElement(host_under_hosts, 'proxy')
 
@@ -142,7 +142,8 @@ def generate_items_xml_file_complete(alarm_list, host_name, host_group_name, hos
 
     SubElement(host_under_hosts, 'inventory')
 
-    return  zabbix_export
+    return zabbix_export
+
 
 def item_creator_type_17_oid(items, host_name, triggers, alarm_values):
     item = SubElement(items, 'item')
@@ -192,7 +193,7 @@ def item_creator_type_17_oid(items, host_name, triggers, alarm_values):
     name.text = 'An Alarm Notification For : ' + alarm_values['name']
     type.text = '17'
     multiplier.text = '0'
-    key.text = 'snmptrap["'+ alarm_values['oid'] +'"]'
+    key.text = 'snmptrap["' + alarm_values['oid'] + '"]'
     delay.text = '0'
     history.text = '90'
     trends.text = '365'
@@ -221,13 +222,13 @@ def item_creator_type_17_oid(items, host_name, triggers, alarm_values):
     trigger_type = SubElement(trigger, 'type')
     SubElement(trigger, 'dependencies')
 
-    trigger_expression.text = '{' + host_name + ':'+ key.text +'.str("' + alarm_values['oid'] + '")}=1'
-    trigger_name.text = 'ATTENTION : On {HOST.NAME}, An Alarm : ' + alarm_values['name'] + ', From Module : ' +alarm_values['mib_module']
+    trigger_expression.text = '{' + host_name + ':' + key.text + '.str("' + alarm_values['oid'] + '")}=1'
+    trigger_name.text = 'ATTENTION : On {HOST.NAME}, An Alarm : ' + alarm_values['name'] + ', From Module : ' + \
+                        alarm_values['mib_module']
     trigger_status.text = '0'
     trigger_priority.text = '3'
     trigger_description.text = description.text
     trigger_type.text = '0'
-
 
 
 def item_creator_type_17(items, host_name, triggers, alarm_values):
@@ -278,7 +279,8 @@ def item_creator_type_17(items, host_name, triggers, alarm_values):
     name.text = 'An Alarm Notification For : ' + alarm_values['name']
     type.text = '17'
     multiplier.text = '0'
-    key.text = 'snmptrap["(' + alarm_values['mib_module'] + '::' + alarm_values['name'] + ')((.|[[:space:]])*)({#SNMPVALUE})"]'
+    key.text = 'snmptrap["(' + alarm_values['mib_module'] + '::' + alarm_values[
+        'name'] + ')((.|[[:space:]])*)({#SNMPVALUE})"]'
     delay.text = '0'
     history.text = '90'
     trends.text = '365'
@@ -307,8 +309,9 @@ def item_creator_type_17(items, host_name, triggers, alarm_values):
     trigger_type = SubElement(trigger, 'type')
     SubElement(trigger, 'dependencies')
 
-    trigger_expression.text = '{' + host_name + ':'+ key.text +'.str(' + alarm_values['name'] + ')}=1'
-    trigger_name.text = 'ATTENTION : On {HOST.NAME}, An Alarm : ' + alarm_values['name'] + ' - {#SNMPVALUE}, From Module : ' +alarm_values['mib_module']
+    trigger_expression.text = '{' + host_name + ':' + key.text + '.str(' + alarm_values['name'] + ')}=1'
+    trigger_name.text = 'ATTENTION : On {HOST.NAME}, An Alarm : ' + alarm_values[
+        'name'] + ' - {#SNMPVALUE}, From Module : ' + alarm_values['mib_module']
     trigger_status.text = '0'
     trigger_priority.text = '3'
     trigger_description.text = description.text
@@ -323,10 +326,11 @@ def xml_pretty_me(file_name_for_prettify, string_to_prettify):
 
     xml = minidom.parseString(string_to_prettify)
     pretty_xml_as_string = xml.toprettyxml()
-    output_file = open(file_name_for_prettify, 'w' )
+    output_file = open(file_name_for_prettify, 'w')
     output_file.write(pretty_xml_as_string)
     logging.debug("Creation Complete")
     output_file.close()
+
 
 def read_from_csv(csv_file_name):
     try:
@@ -347,9 +351,7 @@ def zabbix_snmp_trap_import_from_csv(file_name, host_name, host_group_name, host
         if alarm_data[0] == "Name":
             continue
 
-        oid_dictionary = {}
-        oid_dictionary['name'] = alarm_data[0]
-        oid_dictionary['full_name'] = alarm_data[1]
+        oid_dictionary = {'name': alarm_data[0], 'full_name': alarm_data[1]}
 
         oid_to_convert = str(alarm_data[2])[1:]
         smi_name = get_smi_number_to_name(oid_to_convert[:11])
@@ -368,8 +370,6 @@ def zabbix_snmp_trap_import_from_csv(file_name, host_name, host_group_name, host
 
         alarm_list.append(oid_dictionary)
 
-
-
     xml_tree = generate_items_xml_file_complete(alarm_list, host_name, host_group_name, host_interface_ip)
     xml_tree_as_string = ElementTree.tostring(xml_tree)
     return xml_tree_as_string
@@ -384,11 +384,13 @@ if __name__ == '__main__':
                                                     We use this information to create snmptrap items and corresponding Trigger in the XML.
                                                     Which can be imported directly.''')
 
-
     parser.add_argument('-e', '--export-csv', help='OID file, Gives all OIDs on the device', required=True)
     parser.add_argument('-n', '--host-name', help='Host name as given in Zabbix server.', required=True)
-    parser.add_argument('-g', '--host-group', help='Host Group which the host belongs to, as in Zabbix server.', required=True)
-    parser.add_argument('-i', '--host-interface', help='SNMP Interface configured on Zabbix server. (Assuming Single Interface in Configured)', required=True)
+    parser.add_argument('-g', '--host-group', help='Host Group which the host belongs to, as in Zabbix server.',
+                        required=True)
+    parser.add_argument('-i', '--host-interface',
+                        help='SNMP Interface configured on Zabbix server. (Assuming Single Interface in Configured)',
+                        required=True)
     parser.add_argument('-d', '--debug', help='Running Debug mode - More Verbose', action="store_true")
     parser.add_argument('-v', '--verbose', help='Running Debug mode - More Verbose', action="store_true")
     args = parser.parse_args()
@@ -398,12 +400,12 @@ if __name__ == '__main__':
     zabbix_host_group_name = args.host_group
     zabbix_host_interface_ip = args.host_interface
 
-
     if args.debug or args.verbose:
         logging.basicConfig(level=logging.DEBUG)
     else:
         logging.basicConfig(level=logging.INFO)
 
-    xml_tree_gen_as_string = zabbix_snmp_trap_import_from_csv(csv_file_name, zabbix_host_name, zabbix_host_group_name, zabbix_host_interface_ip)
-    xml_pretty_me(zabbix_host_name.lower()+'-item-trigger-import.xml', xml_tree_gen_as_string)
+    xml_tree_gen_as_string = zabbix_snmp_trap_import_from_csv(csv_file_name, zabbix_host_name, zabbix_host_group_name,
+                                                              zabbix_host_interface_ip)
+    xml_pretty_me(zabbix_host_name.lower() + '-item-trigger-import.xml', xml_tree_gen_as_string)
 
